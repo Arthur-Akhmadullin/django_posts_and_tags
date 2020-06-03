e5a6bd072cb60612c4e2bd5464d204d729e91456
Arthur Akhmadullin <akhmadullinartur@yandex.ru>
2020-06-04 00:41:03 +0500

    Создал базовый шаблон base.html и подключил бутстрап верстку и стили

--- pt/views.py
+++ pt/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
 
 def posts_list(request):
-	return render(request, 'pt/index.html')
+	msg_hello = ["Здорово, бакланы!", "Превед медвед!", "Смешарики, в бой!!!"]
+	return render(request, 'pt/index.html', {"messages": msg_hello})
+	
+	
