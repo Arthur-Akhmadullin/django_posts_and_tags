679aff8b4d7a7a8c9c902fbb6a24822f362617c3
artur_a <artur_alpha87@mail.ru>
2020-06-13 15:38:41 +0500

    Добавил механизм открытия каждого поста

--- pt/views.py
+++ pt/views.py
@@ -3,6 +3,8 @@ from .models import Post
 
 def posts_list(request):
 	posts = Post.objects.all()
-	return render(request, 'pt/index.html', {"posts": posts})
+	return render(request, 'pt/index.html', {'posts': posts})
 	
-	
+def post_detail(request, slug):
+	post = Post.objects.get(slug__iexact=slug)
+	return render(request, 'pt/post_detail.html', {'post': post})
