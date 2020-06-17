30fef4793a45cf37c7f4f9736186532cb1b75551
Arthur Akhmadullin <akhmadullinartur@yandex.ru>
2020-06-17 22:11:20 +0500

    Добавил модель Tag и сделал tags_list.html

--- pt/views.py
+++ pt/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Post
+from .models import Post, Tag
 
 def posts_list(request):
 	posts = Post.objects.all()
@@ -8,3 +8,7 @@ def posts_list(request):
 def post_detail(request, slug):
 	post = Post.objects.get(slug__iexact=slug)
 	return render(request, 'pt/post_detail.html', {'post': post})
+	
+def tags_list(request):
+	tags = Tag.objects.all()
+	return render(request, 'pt/tags_list.html', {'tags': tags})
