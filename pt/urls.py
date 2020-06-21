f734477b43864332c5ce492b6550a900773491b4
Arthur Akhmadullin <akhmadullinartur@yandex.ru>
2020-06-22 02:06:32 +0500

    во views.py замена на классы PostDetail и TagDetail

--- pt/urls.py
+++ pt/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
 from .views import *
 
+'''
+Вместо метода post_detail установлен PostDetail.as_view()
+Вместо метода tag_detail установлен TagDetail.as_view()
+'''
+
 urlpatterns = [
     path('', posts_list, name='posts_list_url'),
-    path('post/<str:slug>/', post_detail, name='post_detail_url'),
+    path('post/<str:slug>/', PostDetail.as_view(), name='post_detail_url'),
 	path('tags/', tags_list, name='tags_list_url'),
-	path('tag/<str:slug>/', tag_detail, name='tag_detail_url'),
+	path('tag/<str:slug>/', TagDetail.as_view(), name='tag_detail_url'),
 ]
--- pt/views.py
+++ pt/views.py
@@ -1,18 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import View
+
 from .models import Post, Tag
+
 
 def posts_list(request):
 	posts = Post.objects.all()
 	return render(request, 'pt/index.html', {'posts': posts})
 	
-def post_detail(request, slug):
-	post = Post.objects.get(slug__iexact=slug)
-	return render(request, 'pt/post_detail.html', {'post': post})
+
+'''
+Функционал методов post_detail и tag_detail перенесен в классы PostDetail и TagDetail
+'''	
+#def post_detail(request, slug):
+	#post = Post.objects.get(slug__iexact=slug)
+	#return render(request, 'pt/post_detail.html', {'post': post})
+	
+class PostDetail(View):
+	def get(self, request, slug):
+		post = get_object_or_404(Post, slug__iexact=slug)
+		return render(request, 'pt/post_detail.html', {'post': post})
+		
 	
 def tags_list(request):
 	tags = Tag.objects.all()
 	return render(request, 'pt/tags_list.html', {'tags': tags})
+
+
+#def tag_detail(request, slug):
+	#tag = Tag.objects.get(slug__iexact=slug)
+	#return render(request, 'pt/tag_detail.html', {'tag': tag})
 	
-def tag_detail(request, slug):
-	tag = Tag.objects.get(slug__iexact=slug)
-	return render(request, 'pt/tag_detail.html', {'tag': tag})
+class TagDetail(View):
+	def get(self, request, slug):
+		tag = get_object_or_404(Tag, slug__iexact=slug)
+		return render(request, 'pt/tag_detail.html', {'tag': tag})
+	
